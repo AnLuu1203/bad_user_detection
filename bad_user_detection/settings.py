ee56df69cf9d26f08984268c610204a1383d04c5
An Luu <lta.anluu@gmail.com>
2017-10-21 20:16:17 +0700

    using mysql

--- bad_user_detection/settings.py
+++ bad_user_detection/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bad_user_detection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bad_user_sendo',
+        'USER': 'root',
+        'PASSWORD': '123123123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
